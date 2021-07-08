35a84d3a612f8f57791bbed5b0973b0143eeae3f
Simon Gagnon <gagnonbsimon@gmail.com>
2021-07-07 20:43:08 -0400

    added  burger nav

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log("test");
+const burger = document.querySelector(".burger");
+const headerNav = document.querySelector(".header__links");
+
+burger.addEventListener("click", function () {
+
+    if(!burger.classList.contains('open')) {
+        burger.classList.add('open');
+        headerNav.style.display = "block";
+    } else {
+        burger.classList.remove('open');
+        headerNav.style.display = "none";
+    }
+});
